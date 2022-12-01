3857babe477b6ddeb6b211283feec7c6ec474923
zhu-yuncheng <zhuyuncheng@huawei.com>
2022-12-02 01:30:35 +0800

    update spec and setup.py

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     data_files=[
         ('/etc/aops', ['conf/diana.ini', 'conf/diana_hosts.json']),
         ('/etc/aops/algorithm', ['conf/algorithm/mysql_intelligent.json']),
-        ('/etc/aops/algorithm', ['conf/algorithm/ai_template1.json', 'conf/algorithm/ai_template1.json']),
+        ('/etc/aops/algorithm', ['conf/algorithm/ai_template1.json', 'conf/algorithm/ai_template2.json']),
         ('/etc/aops/algorithm', ['conf/algorithm/lvs_network_error_tree.json']),
         ('/etc/aops/algorithm', ['conf/algorithm/mysql_network_error_tree.json']),
         ('/etc/aops/algorithm', ['conf/algorithm/tpcc_network_error_tree.json']),
