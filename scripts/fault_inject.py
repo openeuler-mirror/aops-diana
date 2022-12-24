8fb1e1cbe8df3fdfe82b253984903e7d5e5948f3
wang-guangge <wangguangge@huawei.com>
2022-12-24 09:54:30 +0800

    remove config file and edit cleartext password

--- scripts/fault_inject.py
+++ scripts/fault_inject.py
@@ -8,8 +8,44 @@ from functools import partial
 import paramiko
 import threading
 import logging
-from config import CONFIG
 import re
+
+CONFIG = {
+        'tpcc': {
+            'ip': '172.168.193.208',
+            'interface': 'enp1s0',
+            'chaosblade_port': 7070,
+            'benchmarksql': {
+                'run_filedir': '/home/wgg/software/benchmarksql5.0-for-mysql/run/',
+                'run_cmd': '/home/wgg/software/benchmarksql5.0-for-mysql/run/runBenchmark.sh props.conf > /home/wgg/software/benchmarksql5.0-for-mysql/run/run.log',
+                'log_file': '/home/wgg/software/benchmarksql5.0-for-mysql/run/run.log'
+                }
+            },
+        'mysqlA': {
+            'ip': '172.168.108.236',
+            'interface': 'enp1s0',
+            'chaosblade_port': 7070
+            },
+        'mysqlB': {
+            'ip': '172.168.108.112',
+            'interface': 'enp1s0',
+            'chaosblade_port': 7070
+            },
+        'VIP': '172.168.108.134',
+        'lvs': {
+            'ip': '172.168.108.111',
+            'interface': 'enp1s0',
+            'chaosblade_port': 7070
+            },
+        'aops': {
+            'ip': '172.168.201.40',
+            'interface': 'enp1s0',
+            'chaosblade_port': 7070
+            },
+        'fault_inject_file': './test_fault_inject_recording.txt',
+        'fault_inject_log': './test_fault_inject_recording.log'
+        }
+
 
 def get_node_name(fault_node):
     node_name = {
@@ -69,7 +105,7 @@ def chaos_inject_network(host, port, params, time_out, fault_node, fault_type):
 def ssh_cmd(target_host, cmd):
     client = paramiko.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    client.connect(hostname=target_host, username='root', password='openeuler@123')
+    client.connect(hostname=target_host, username='USERNAME', password='PASSWORD')
     try:
         c = ""
         for c in cmd:
@@ -93,7 +129,7 @@ def check_mysql_running(mysql_ip):
     
     # check if mysql is running by 'ipvsadm -ln' on lvs
     lvs_ip = CONFIG['lvs']['ip']
-    client.connect(hostname=lvs_ip, username='root', password='openeuler@123')
+    client.connect(hostname=lvs_ip, username='USERNAME', password='PASSWORD')
     mysql_status_check_cmd = 'ipvsadm -ln'
     stdin, stdout, stderr = client.exec_command(mysql_status_check_cmd)
     
@@ -115,7 +151,7 @@ def check_mysql_running(mysql_ip):
 def check_tpcc_status(tpcc_ip):
     client = paramiko.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    client.connect(hostname=tpcc_ip, username='root', password='openeuler@123')
+    client.connect(hostname=tpcc_ip, username='USERNAME', password='PASSWORD')
     try:
         # wait for the state to stabilize
         time.sleep(30)
